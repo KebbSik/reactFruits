53d4981816c2d7ded2b0ce48aae9db51b2a37a5c
Kevin <kevinn.sarfoo1@gmail.com>
2025-04-15 22:14:24 +0200

    Making navbar responsive and add global smooth color transitions

--- src/FruitsMap.ts
+++ src/FruitsMap.ts
@@ -1,9 +1,9 @@
 export const themeMap = {
-    banana: { color: "#e8ca0c", textColor: "#fff" },
-    watermelon: { color: "#e75fa6", textColor: "#fff" },
-    apple: { color: "#1fc205", textColor: "#fff" },
-    strawberry: { color: "#e33502", textColor: "#fff" },
-    peach: { color: "#f66a09", textColor: "#fff" },
+    banana: { color: "#e8ca0c", textColor: "#fff",transparentColor: "#e8ca0ccc" },
+    watermelon: { color: "#e75fa6", textColor: "#fff",transparentColor: "#e75fa6cc" },
+    apple: { color: "#1fc205", textColor: "#fff",transparentColor: "#1fc205cc" },
+    strawberry: { color: "#e33502", textColor: "#fff",transparentColor: "#e33502cc" },
+    peach: { color: "#f66a09", textColor: "#fff",transparentColor: "#f66a09cc" },
   } as const;
   
   export type Fruit = keyof typeof themeMap;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
 import { div, feSpotLight } from "motion/react-client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Fruit } from "../FruitsMap";
 import { addScaleCorrector, easeInOut, motion } from "motion/react";
 import useMaxScreenSize from "../useMaxScreenSize";
 import { useTheme } from "./ThemeContext";
 import { fruitList } from "../FruitsMap";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   const { max480, max768, max992, max1280, max1400 } = useMaxScreenSize();
   const [isActive, setIsActive] = useState(false);
   const { theme, changeFruit } = useTheme();
+
+  useEffect(() => {
+    if (!max768) {
+      setIsActive(false);
+    }
+  }, [max768]);
   return (
     <>
       {!max768 && (
         <nav
           style={{
             display: "flex",
-            // flexDirection: max768 ? "column" : "row",
             flexDirection: "row",
-            // gap: max768 ? "1rem" : "",
             alignItems: "center",
             justifyContent: "space-around",
             width: "100%",
@@ -28,25 +33,6 @@ const Navbar = () => {
             padding: ".6rem",
           }}
         >
-          {/* {max768 && (
-          <div
-            style={{
-              height: "2rem",
-              display: "flex",
-              width: "100%",
-              justifyContent: "flex-end",
-            }}
-          >
-            <button
-              onClick={() => {
-                setIsActive(!isActive);
-                console.log(isActive);
-              }}
-            >
-              Burger
-            </button>
-          </div>
-        )} */}
           {fruitList.map((fruit) => (
             <motion.a
               whileHover={{ scale: 1.1, rotate: [-3, 3] }}
@@ -70,8 +56,8 @@ const Navbar = () => {
           style={{
             display: "flex",
             flexDirection: "column",
-            gap: max768 ? "1rem" : "",
-            alignItems: "center",
+            // gap: max768 ? "1rem" : "",
+            // alignItems: "center",
             justifyContent: "space-around",
             width: "100%",
             minHeight: "4rem",
@@ -87,35 +73,62 @@ const Navbar = () => {
                 display: "flex",
                 width: "100%",
                 justifyContent: "flex-end",
+                alignItems: "center",
               }}
             >
-              <button
-                onClick={() => {
-                  setIsActive(!isActive);
-                  console.log(isActive);
-                }}
-              >
-                Burger
-              </button>
+              <motion.div animate={isActive ? { rotate: -90 } : { rotate: 0 }}>
+                <GiHamburgerMenu
+                  style={{
+                    fontSize: max480 ? "2rem" : "2.5rem",
+                  }}
+                  onClick={() => {
+                    setIsActive(!isActive);
+                    console.log(isActive);
+                  }}
+                ></GiHamburgerMenu>
+              </motion.div>
             </div>
           )}
-          {fruitList.map((fruit) => (
-            <motion.a
-              whileHover={{ scale: 1.1, rotate: [-3, 3] }}
-              whileTap={{ scale: 0.8, transition: { duration: 0.1 } }}
-              transition={{ duration: 0.1, ease: easeInOut }}
-              style={{
-                scale: isActive ? 1 : 0,
-                fontSize: max992 ? "1rem" : "1.3rem",
-                padding: max768 ? "0.5rem" : "",
-                cursor: "pointer",
-              }}
-              onClick={() => changeFruit(fruit)}
-              key={fruit}
-            >
-              {fruit.charAt(0).toUpperCase() + fruit.slice(1)}
-            </motion.a>
-          ))}
+          <motion.div
+            id="mobileNav"
+            initial={{ height: 0, opacity: 0 }}
+            animate={
+              isActive
+                ? { height: "auto", opacity: 1, y: -5 }
+                : { height: 0, opacity: 1, y: -10 }
+            }
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            style={{
+              position: "absolute",
+              top: "4rem",
+              left: 0,
+              width: "100%",
+              backgroundColor: theme.transparentColor,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backdropFilter: "blur(4px)",
+              borderRadius: "0 0 3% 3%",
+              // height: isActive ? "" : 0,
+            }}
+          >
+            {fruitList.map((fruit) => (
+              <motion.a
+                whileHover={{ scale: 1.1, rotate: [-3, 3] }}
+                whileTap={{ scale: 0.8, transition: { duration: 0.1 } }}
+                transition={{ duration: 0.1, ease: easeInOut }}
+                style={{
+                  fontSize: max480 ? "1rem" : "1.3rem",
+                  padding: "0.8rem",
+                  cursor: "pointer",
+                }}
+                onClick={() => changeFruit(fruit)}
+                key={fruit}
+              >
+                {fruit.charAt(0).toUpperCase() + fruit.slice(1)}
+              </motion.a>
+            ))}
+          </motion.div>
         </nav>
       )}
     </>
